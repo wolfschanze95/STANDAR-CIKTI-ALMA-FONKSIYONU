d6ff7887e6f558847ded9fe0faadd97822102361
wolfschanze95 <rlpatlgan.01@gmail.com>
2024-11-07 16:40:46 +0300

    Create example.py

---
+++ example.py
@@ -0,0 +1,6 @@
+isim="Mehmet Eralp Atilgan"
+yas=16
+meslek="öğrenci"
+bolum="sayisal"
+okul="Adana Anadolu Lisesi"
+print("isminiz:",isim,"yaşınız:",yas,"mesleğiniz:",meslek,"bölümünüz",bolum,"okulunuz",okul)
